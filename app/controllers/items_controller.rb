df7615e9cb7fb1f299eb89fc0125ae5d2b64e768
s-itonaga <sitonaga914@gmail.com>
2020-09-25 22:00:39 +0900

    Revert "間違えてコミット"
    This reverts commit 4036dbe8d9f56664e1f938f4b89ca66800b1b8bc.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,4 @@
 class ItemsController < ApplicationController
-
-  # before_action :move_to_index, expect: [ :index ]
-  
   def index
   end
-
-  # private
-
-  # def move_to_index
-  #   unless user_signed_in?
-  #     redirect_to action: :index
-  #   end
-  # end
-
 end
