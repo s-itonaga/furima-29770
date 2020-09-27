8524a9558ce8cbbc199d8152b5c9806128b76797
s-itonaga <sitonaga914@gmail.com>
2020-09-27 22:59:19 +0900

    rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                  {"砂糖"}
-    email                     {"test@gmail.com"}
-    password                  {"123456"}
-    password_confirmation     {password}
-    family_name               {"佐藤"}
-    name                      {"孝雄"}
-    pronuciation_family_name  {"サトウ"}
-    pronuciation_name         {"タカオ"}
-    birth_date                {"1976-09-14"}
+    nickname                  { '砂糖' }
+    email                     { 'test@gmail.com' }
+    password                  { '123456' }
+    password_confirmation     { password }
+    family_name               { '佐藤' }
+    name                      { '孝雄' }
+    pronuciation_family_name  { 'サトウ' }
+    pronuciation_name         { 'タカオ' }
+    birth_date                { '1976-09-14' }
   end
 end
