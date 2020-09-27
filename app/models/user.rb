492f544e00b595d1215a2c0628ed062ef898d8aa
s-itonaga <sitonaga914@gmail.com>
2020-09-27 16:39:37 +0900

    テストコード準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,                  presence: true
+         validates :family_name,               presence: true
+         validates :name,                      presence: true
+         validates :pronuciation_family_name,  presence: true
+         validates :pronuciation_name,         presence: true
+         validates :birth_dote,                presence: true
 end
