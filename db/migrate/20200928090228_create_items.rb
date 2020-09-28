2b2d4a0a2f4cabaaf75dd64bbc6b1720203dd18a
s-itonaga <sitonaga914@gmail.com>
2020-09-28 18:33:50 +0900

    マイグレーションを編集

--- db/migrate/20200928090228_create_items.rb
+++ db/migrate/20200928090228_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,      null: false
+      t.text :comment,     null: false
+      t.integer :catrgory, null: false
+      t.integer :delivery_fee, null: false
+      t.integer :prefectures, null: false
+      t.integer :delivery_days, null: false
+      t.integer :price,         null: false
+      t.integer :user_id
       t.timestamps
     end
   end
