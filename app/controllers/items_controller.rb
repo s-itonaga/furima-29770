7c35087754c3e7a094c4d52a3cafec1d8b24055e
s-itonaga <sitonaga914@gmail.com>
2020-09-29 16:03:52 +0900

    createアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,14 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require( :item ).permit( :name, :comment, :category, :status, :delivery_fee, :prefectures, :delivery_days, :price ).merge( user_id: current_user.id )
+  end
+
 end
