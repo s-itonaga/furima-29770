f61e9bf550711e792fcc67408bde79f225547949
s-itonaga <sitonaga914@gmail.com>
2020-09-29 15:39:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,17 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :comment
+    validates :category
+    validates :status
+    validates :delivery_fee
+    validates :prefectures
+    validates :price
+  end
+
+    validates :category, numericality: { other_than: 1 }
+
 end
