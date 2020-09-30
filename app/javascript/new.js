5cc5c3a4b8060ac9a1da754aeb13e53a4ac30acf
s-itonaga <sitonaga914@gmail.com>
2020-09-30 15:49:05 +0900

    販売手数料と利益の表示修正

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -6,10 +6,10 @@ function SalesCommission(){
     // console.log(price.value);
     const sale = (price.value)*0.1;
     const fee = document.getElementById('add-tax-price');
-    fee.innerHTML = `${sale}`;
+    fee.innerHTML = `${(sale.toFixed())}`;
     const profit = (price.value)*0.9;
     const prf = document.getElementById('profit');
-    prf.innerHTML = `${profit}`;
+    prf.innerHTML = `${(profit.toFixed())}`;
   });
 }
 
