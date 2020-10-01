6fbe4e7afa1eb00a69bfd916f139c20e8f49f3d8
s-itonaga <sitonaga914@gmail.com>
2020-10-01 20:11:20 +0900

    rubocopで修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,7 +57,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it '価格の範囲が、¥300~¥9,999,999の間であること(100000000円以上で入力)' do
-      @item.price = 100000000
+      @item.price = 100_000_000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
