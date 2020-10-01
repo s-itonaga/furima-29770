7d8d724edcb3ee6a8f9b07dca178ce07a2e5b415
s-itonaga <sitonaga914@gmail.com>
2020-10-01 12:26:11 +0900

    テストコードの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image,             { '' }
+    name,              { 'メルセデス' }
+    comment,           { 'お買い得品' }
+    category_id,       { '2' }
+    status_id,         { '2' }
+    delivery_fee_id,   { '2' }
+    prefecture_id,     { '2' }
+    delivery_day_id,   { '2' }
+    price,             { '1000' }
+    user_id            { '8' }
   end
 end
