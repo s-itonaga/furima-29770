0f4d79106617f0c7daba494ccf1869e0e49bd1bc
s-itonaga <sitonaga914@gmail.com>
2020-10-01 17:35:52 +0900

    コメントアウト削除

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,9 +1,7 @@
 function SalesCommission(){
 
   const price = document.getElementById('item-price')
-  // console.log(price)
     price.addEventListener("keyup",() => {
-    // console.log(price.value);
     const sale = (price.value)*0.1;
     const fee = document.getElementById('add-tax-price');
     fee.innerHTML = `${(sale.toFixed())}`;
