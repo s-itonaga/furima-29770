d2e8e322cae73bf4bffa8e1eb721fc056eb5ccc6
s-itonaga <sitonaga914@gmail.com>
2020-10-01 14:41:38 +0900

    テストコードを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :item do
-    image,             { '' }
-    name,              { 'メルセデス' }
-    comment,           { 'お買い得品' }
-    category_id,       { '2' }
-    status_id,         { '2' }
-    delivery_fee_id,   { '2' }
-    prefecture_id,     { '2' }
-    delivery_day_id,   { '2' }
-    price,             { '1000' }
-    user_id            { '8' }
+    name              { 'メルセデス' }
+    comment           { 'お買い得品' }
+    category_id       { '2' }
+    status_id         { '2' }
+    delivery_fee_id   { '2' }
+    prefecture_id     { '2' }
+    delivery_day_id   { '2' }
+    price             { '1000' }
+    association :user
   end
 end
