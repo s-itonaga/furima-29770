a9540a9ef30f40fa33d8fe05a66e6d648bba0e76
s-itonaga <sitonaga914@gmail.com>
2020-10-01 17:41:42 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name              { 'メルセデス' }
     comment           { 'お買い得品' }
-    category_id       { '2' }
-    status_id         { '2' }
-    delivery_fee_id   { '2' }
-    prefecture_id     { '2' }
-    delivery_day_id   { '2' }
-    price             { '1000' }
+    category_id       { 2 }
+    status_id         { 2 }
+    delivery_fee_id   { 2 }
+    prefecture_id     { 2 }
+    delivery_day_id   { 2 }
+    price             { 1000 }
     association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,27 +22,27 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Comment can't be blank")
     end
     it 'カテゴリーの情報が必須であること' do
-      @item.category_id = '1'
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
     it '商品の状態についての情報が必須であること' do
-      @item.status_id = '1'
+      @item.status_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Status must be other than 1')
     end
     it '配送料の負担についての情報が必須であること' do
-      @item.delivery_fee_id = '1'
+      @item.delivery_fee_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Delivery fee must be other than 1')
     end
     it '発送元の地域についての情報が必須であること' do
-      @item.prefecture_id = '1'
+      @item.prefecture_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
     end
     it '発送までの日数についての情報が必須であること' do
-      @item.delivery_day_id = '1'
+      @item.delivery_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include('Delivery day must be other than 1')
     end
@@ -52,12 +52,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number')
     end
     it '価格の範囲が、¥300~¥9,999,999の間であること(299円以下で入力)' do
-      @item.price = '299'
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it '価格の範囲が、¥300~¥9,999,999の間であること(100000000円以上で入力)' do
-      @item.price = '100000000'
+      @item.price = 100000000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
