a17a3d0effa1b954540495744786c6ca7bb7766f
s-itonaga <sitonaga914@gmail.com>
2020-10-02 11:05:20 +0900

    ルーティングにshowを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resource :items, only: [:index, :new, :create]
+  resource :items, only: [:index, :new, :create, :show]
 end
