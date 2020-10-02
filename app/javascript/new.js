d8f7b61c3ea4c6bc03429bd25784bdf2617cd831
s-itonaga <sitonaga914@gmail.com>
2020-10-02 18:34:50 +0900

    form価格の記述変更

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,7 +2,7 @@ function SalesCommission(){
 
   const price = document.getElementById('item-price')
     price.addEventListener("keyup",() => {
-    const sale = (price.value)*0.1;
+    const sale = ((price.value)*0.1);
     const fee = document.getElementById('add-tax-price');
     fee.innerHTML = `${(sale.toFixed())}`;
     const profit = (price.value)*0.9;
