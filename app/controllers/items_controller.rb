df2e4350f0eae3e439766c1a44429d0a806fe88a
s-itonaga <sitonaga914@gmail.com>
2020-10-02 20:38:06 +0900

    updateアクションの記述ミスを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,8 +25,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to show
     else
       render :edit
