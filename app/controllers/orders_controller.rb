116344b2b670646ddfd10beeb1828dbb6ed55bcd
s-itonaga <sitonaga914@gmail.com>
2020-10-07 14:38:54 +0900

    createアクションを編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
-    if @order.save
-      redirect_to root_path
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -18,6 +20,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id, token: params[:token])
+    params.permit(:token,:item_id).merge(user_id: current_user.id)
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
