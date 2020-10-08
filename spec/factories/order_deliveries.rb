13c4886d380cec5c18d6699ee7d8f791630717bc
s-itonaga <sitonaga914@gmail.com>
2020-10-08 18:06:42 +0900

    テストコードを記述

--- spec/factories/order_deliveries.rb
+++ spec/factories/order_deliveries.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     city           { '宮崎市' }
     address        { '花が島町12丁目３４番地' }
     building_name  { '柳ビル101' }
-    phone_number　　{ '09011110000' }
+    phone_number   { '09011110000' }
   end
 end
