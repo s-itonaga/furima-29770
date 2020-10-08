76e0c43ebad53cfa3c9f95627aad75259b3cb94d
s-itonaga <sitonaga914@gmail.com>
2020-10-08 16:36:50 +0900

    order_deliveryモデルを作成

---
+++ spec/factories/order_deliveries.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_delivery do
+    postal_code    { '123-4567' }
+    prefecture_id  { 2 }
+    city           { '宮崎市' }
+    address        { '花が島町12丁目３４番地' }
+    building_name  { '柳ビル101' }
+    phone_number　　{ '09011110000' }
+  end
+end
