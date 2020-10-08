8768aa79d4a0f1980dc12c2261de5c0ed4b08128
s-itonaga <sitonaga914@gmail.com>
2020-10-08 12:28:08 +0900

    出品者が自身の購入ページへは入れない様に記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_item
   before_action :move_to_rogin
+  before_action :move_to_root
   def index
     @order = Order.new
   end
@@ -17,6 +18,12 @@ class OrdersController < ApplicationController
   end
 
   private
+  def move_to_root
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
   def move_to_rogin
     unless user_signed_in?
       redirect_to new_user_session_path
