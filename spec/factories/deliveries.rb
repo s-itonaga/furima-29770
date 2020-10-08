598e24526d61246e3e8aa73a1c1b4ae9d302b202
s-itonaga <sitonaga914@gmail.com>
2020-10-08 16:05:51 +0900

    FactoryBotの記述を編集

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :delivery do
-    
+    postal_code    { '123-4567' }
+    prefecture_id  { 2 }
+    city           { '○○市' }
+    address        { '××町12丁目３４番地' }
+    building_name  { '△△ビル101' }
+    phone_number　　{ 09011110000 }
   end
 end
